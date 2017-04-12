7f032606c31d4632880c8da600daacca0e544abc
seawind8888 <344023858@qq.com>
2017-04-12 18:50:32 +0800

    aaaaaa

--- src/page/classDetail.vue
+++ src/page/classDetail.vue
@@ -15,7 +15,7 @@
     <section class="class-content-container">
       <div class="section-title-container">
         <div class="title-container">
-          <i class="title-icon"></i> 已选课程
+          <i class="title-icon"></i> 课程介绍
         </div>
       </div>
       <div class="class-content">
@@ -32,7 +32,7 @@
     <section class="class-list-container">
       <div class="section-title-container">
         <div class="title-container">
-          <i class="title-icon"></i> 已选课程
+          <i class="title-icon"></i> 课程信息
         </div>
       </div>
       <ul class="list-item-container">
@@ -62,7 +62,7 @@
         </li>
         <li class="class-list-item">
           <div class="list-left">
-            <img class="list-icon" src="../assets/imgs/classDetails/class-list-icon-3.png" />
+            <img class="list-icon long-icon" src="../assets/imgs/classDetails/class-list-icon-3.png" />
           </div>
           <div class="list-right">
             <div class="right-title">课程地点</div>
@@ -98,6 +98,92 @@
         </li>
       </ul>
     </section>
+    <section>
+      <div class="section-title-container">
+        <div class="title-container">
+          <i class="title-icon"></i> 关联的Club
+        </div>
+        <div class="title-container-right">
+          查看全部
+          <img src="../assets/imgs/classDetails/class-list-enter-icon.png">
+        </div>
+      </div>
+      <div class="club-list-container">
+        <div class="class-item-container">
+          <div class="item-container">
+            <div class="item-left">
+              <img src='https://ooo.0o0.ooo/2017/04/08/58e8b43ad64b9.png'></div>
+            <div class="item-middle">
+              <div class="item-title-container">欧美幼儿俱乐部</div>
+              <div class="item-info-container">
+                时间:&nbsp2017.01.01（周一）10-12点
+              </div>
+              <div class="item-info-container">
+                地点:&nbsp三里屯soho星巴克咖啡厅
+              </div>
+              <div class="item-info-container">
+                外教:&nbspTess
+              </div>
+            </div>
+            <div class="item-right">
+              <img src="../assets/imgs/classDetails/class-list-enter-icon.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="section-title-container">
+        <div class="title-container">
+          <i class="title-icon"></i> 评论
+        </div>
+        <div class="title-container-right">
+          查看更多
+          <img src="../assets/imgs/classDetails/class-list-enter-icon.png">
+        </div>
+      </div>
+      <ul class="evaluate-list-container">
+        <li class="evaluate-list-item">
+          <div class="list-left">
+            <img src="../assets/imgs/App/img_test.png">
+          </div>
+          <div class="list-right">
+            <div class="list-user-name">
+              <span>Anna</span>
+              <div class="reply-icon-container">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+              </div>
+            </div>
+            <div class="list-reply-time">2016-09-09</div>
+            <div class="reply-info">此处是评价文字文字评价评价评价</div>
+          </div>
+        </li>
+                <li class="evaluate-list-item">
+          <div class="list-left">
+            <img src="../assets/imgs/App/img_test.png">
+          </div>
+          <div class="list-right">
+            <div class="list-user-name">
+              <span>Anna</span>
+              <div class="reply-icon-container">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+                <img class="reply-icon" src="../assets/imgs/classDetails/reply-star-icon.png">
+              </div>
+            </div>
+            <div class="list-reply-time">2016-09-09</div>
+            <div class="reply-info">此处是评价文字文字评价评价评价</div>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <footer></footer>
   </div>
 </template>
 <script>
