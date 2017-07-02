ec1e7d2edb0f69f81f5459e87c2b4d1425d6cacf
harvest_wang <344023858@qq.com>
2017-07-02 18:24:43 +0800

    modify

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="login-container">
-    <input id="abc" placeholder="请输入手机号码" class="input-container" />
-    <input placeholder="请输入密码" type="password" class="input-container" />
-    <router-link to="/home" class="login-btn">登录</router-link>
+    <input id="abc" placeholder="请输入手机号码" v-model="userName" class="input-container" />
+    <input placeholder="请输入密码" type="password" v-model="passWord" class="input-container" />
+    <div  @click="singIn()" class="login-btn">登录</div>
     <div class="login-info">点击开始，即表示已阅读并同意<a>《平行国服务协议》</a></div>
   </div>
 </template>
 <script>
+  import axios from 'axios'
+
   export default {
+    data () {
+      return {
+        userName: '',
+        passWord: ''
+      }
+    },
+    methods: {
+      singIn () {
+        axios('/api/auth/login', {
+            method: 'post',
+            headers: {
+              "Accept": "application/json",
+              "Accept-Language": "zh-hans"
+            },
+            data: {
+              mobile: this.userName,
+              password: this.passWord
+            }
+          })
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
+    }
   }
 </script>
 <style lang="scss">
